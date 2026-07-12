af746bdde59d5d1f2f14868433e809f942d2d53a
khushupadhyay1234 <153370470+khushupadhyay1234@users.noreply.github.com>
2026-07-12 12:32:39 +0530

    Update Vehicle.js

--- backend/models/Vehicle.js
+++ backend/models/Vehicle.js
@@ -1,15 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const vehicleSchema = new mongoose.Schema({
-  registrationNumber: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  maxLoadCapacity: { type: Number, required: true },
-  status: { 
-    type: String, 
-    enum: ['Available', 'On Trip', 'In Shop', 'Retired'], 
-    default: 'Available' 
+  registrationNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  name: {
+    type: String,
+    required: true
+  },
+
+  type: {
+    type: String,
+    required: true
+  },
+
+  maxLoadCapacity: {
+    type: Number,
+    required: true
+  },
+
+  odometer: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+
+  acquisitionCost: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+
+  status: {
+    type: String,
+    enum: ["Available", "On Trip", "In Shop", "Retired"],
+    default: "Available"
   }
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('Vehicle', vehicleSchema);
+module.exports = mongoose.model("Vehicle", vehicleSchema);
